5ea4164306c33297c6262c931f22b92220583a09
Toby Inkster <mail@tobyinkster.co.uk>
2013-11-18 21:49:22 +0000

    allow non-Type::Tiny type constraints to be returned from type constraint expressions

--- lib/Kavorka/ReturnType.pm
+++ lib/Kavorka/ReturnType.pm
@@ -76,13 +76,18 @@ sub parse
 	{
 		lex_read(1);
 		lex_read_space;
-		my $expr = parse_listexpr;
+		my $expr = parse_listexpr
+			or croak('Could not parse type constraint expression as listexpr');
 		lex_read_space;
-		lex_peek eq ')' or croak("Expected ')' after type constraint expression");
+		lex_peek eq ')'
+			or croak("Expected ')' after type constraint expression");
 		lex_read(1);
 		lex_read_space;
-		$type = $expr->();
-		$type->isa('Type::Tiny') or croak("Type constraint expression did not return a blessed type constraint object");
+		
+		require Types::TypeTiny;
+		$type = Types::TypeTiny::to_TypeTiny( scalar $expr->() );
+		$type->isa('Type::Tiny')
+			or croak("Type constraint expression did not return a blessed type constraint object");
 	}
 	else
 	{
